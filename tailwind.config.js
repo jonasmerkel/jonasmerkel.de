8cf86b803164649f2b4eba58ca3995de214a7f9b
Jonas <web.jonas3@gmail.com>
2023-01-31 00:52:09 +0100

    tailwind installation and photography page added

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'index.html',
+    'assets/photography/photography.html'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        danc: "'Dancing Script', cursive;",
+        itim: "'Itim', cursive;",
+      }
+    },
+  },
+  plugins: [],
+}
