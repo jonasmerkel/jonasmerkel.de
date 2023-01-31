c2cd1b177e09363819da5a480ae1ce75ee4db52c
Jonas <web.jonas3@gmail.com>
2023-01-31 19:02:00 +0100

    update paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    'index.html',
-    'assets/photography/photography.html'
+    './index.html',
+    'assets/photography/photography.html',
   ],
   theme: {
     extend: {
